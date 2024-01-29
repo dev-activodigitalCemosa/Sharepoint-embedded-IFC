37a13956cc96aec92f61f3b5cb3341c68547dac5
dev-activodigitalCemosa <156947715+dev-activodigitalCemosa@users.noreply.github.com>
2024-01-29 13:43:41 +0100

    Add files via upload

---
+++ src/webparts/ifcSimple/IfcSimpleWebPart.module.scss.ts
@@ -0,0 +1,10 @@
+/* tslint:disable */
+require("./IfcSimpleWebPart.module.css");
+const styles = {
+  sharepointViewer: 'sharepointViewer_5cae7d59',
+  customButton: 'customButton_5cae7d59',
+  buttonHover: 'buttonHover_5cae7d59'
+};
+
+export default styles;
+/* tslint:enable */
